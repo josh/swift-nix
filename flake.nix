2c46815ea714c776bf95413268266912ec3ddd57
Joshua Peek <josh@users.noreply.github.com>
2024-07-22 22:16:07 +0000

    Add swiftly-install

--- flake.nix
+++ flake.nix
@@ -45,42 +45,68 @@
         treefmt = treefmt.${system}.config.build.check self;
       });
 
-      packages.x86_64-linux.swiftly =
-        let
-          pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        in
-        pkgs.stdenv.mkDerivation {
-          name = "swiftly";
-          version = "0.3.0";
-          src = pkgs.fetchurl {
-            url = "https://github.com/swiftlang/swiftly/releases/download/0.3.0/swiftly-x86_64-unknown-linux-gnu";
-            sha256 = "1gll8rq5qrs4wblk8vds9wcfkva0sdmp88kpj2dwvxwjc04x680q";
-          };
-          dontUnpack = true;
-          installPhase = ''
-            mkdir -p $out/bin
-            cp $src $out/bin/swiftly
-            chmod +x $out/bin/swiftly
-          '';
-        };
+      packages =
+        {
+          x86_64-linux.swiftly =
+            let
+              pkgs = nixpkgs.legacyPackages.x86_64-linux;
+            in
+            pkgs.stdenv.mkDerivation {
+              name = "swiftly";
+              version = "0.3.0";
+              src = pkgs.fetchurl {
+                url = "https://github.com/swiftlang/swiftly/releases/download/0.3.0/swiftly-x86_64-unknown-linux-gnu";
+                sha256 = "1gll8rq5qrs4wblk8vds9wcfkva0sdmp88kpj2dwvxwjc04x680q";
+              };
+              dontUnpack = true;
+              installPhase = ''
+                mkdir -p $out/bin
+                cp $src $out/bin/swiftly
+                chmod +x $out/bin/swiftly
+              '';
+            };
 
-      packages.aarch64-linux.swiftly =
-        let
-          pkgs = nixpkgs.legacyPackages.aarch64-linux;
-        in
-        pkgs.stdenv.mkDerivation {
-          name = "swiftly";
-          version = "0.3.0";
-          src = pkgs.fetchurl {
-            url = "https://github.com/swiftlang/swiftly/releases/download/0.3.0/swiftly-aarch64-unknown-linux-gnu";
-            sha256 = "1gll8rq5qrs4wblk8vds9wcfkva0sdmp88kpj2dwvxwjc04x680q";
-          };
-          dontUnpack = true;
-          installPhase = ''
-            mkdir -p $out/bin
-            cp $src $out/bin/swiftly
-            chmod +x $out/bin/swiftly
-          '';
-        };
+          aarch64-linux.swiftly =
+            let
+              pkgs = nixpkgs.legacyPackages.aarch64-linux;
+            in
+            pkgs.stdenv.mkDerivation {
+              name = "swiftly";
+              version = "0.3.0";
+              src = pkgs.fetchurl {
+                url = "https://github.com/swiftlang/swiftly/releases/download/0.3.0/swiftly-aarch64-unknown-linux-gnu";
+                sha256 = "1gll8rq5qrs4wblk8vds9wcfkva0sdmp88kpj2dwvxwjc04x680q";
+              };
+              dontUnpack = true;
+              installPhase = ''
+                mkdir -p $out/bin
+                cp $src $out/bin/swiftly
+                chmod +x $out/bin/swiftly
+              '';
+            };
+        }
+        // forAllSystems (
+          system:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+          in
+          {
+
+            swiftly-install = pkgs.stdenv.mkDerivation {
+              name = "swiftly-install";
+              src = pkgs.fetchurl {
+                url = "https://swiftlang.github.io/swiftly/swiftly-install.sh";
+                sha256 = "m+2G82gj4XjW/pX84AWIuILsbpF8IDKkj+bNThVBFlc=";
+              };
+              dontUnpack = true;
+              installPhase = ''
+                mkdir -p $out/bin
+                cp $src $out/bin/swiftly-install
+                chmod +x $out/bin/swiftly-install
+              '';
+            };
+          }
+        );
+
     };
 }
